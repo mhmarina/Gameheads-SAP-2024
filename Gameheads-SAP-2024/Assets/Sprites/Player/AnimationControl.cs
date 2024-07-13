464ec4673e3331448dd86e0b05840cdbbc0c5dbf
blursified <serenacai24@gmail.com>
2024-07-12 20:07:13 -0700

    controller editing

---
+++ Gameheads-SAP-2024/Assets/Sprites/Player/AnimationControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    public Animator Controller; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) ) 
+        {
+            Controller.Play("Left Cycle");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) ) 
+        {
+            Controller.Play("Right Cycle");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) ) 
+        {
+            Controller.Play("Player Back Cycle");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) ) 
+        {
+            Controller.Play("Front Cycle");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Controller.Play("Breathe In");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            Controller.Play("Breathe Out");
+        }
+    }
+}
