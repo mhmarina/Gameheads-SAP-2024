c62d201aab3f4130d6340b7621b4cfb00ede8616
blursified <serenacai24@gmail.com>
2024-07-12 20:37:17 -0700

    clean up code

--- Gameheads-SAP-2024/Assets/Sprites/Player/AnimationControl.cs
+++ Gameheads-SAP-2024/Assets/Sprites/Player/AnimationControl.cs
@@ -10,13 +10,14 @@ public class AnimationControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //controller call
         Controller = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //gotta find another way to make the animations temporary
         if (Input.GetKeyDown(KeyCode.Space)) //change this to something else
         {
             canMove = true; 
@@ -25,31 +26,37 @@ public class AnimationControl : MonoBehaviour
 
         if (canMove == true)
         { 
+            // left animation trigger
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) ) 
             {
                 Controller.Play("Left Cycle");
             }
 
+            // right animation trigger
             else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) ) 
             {
                 Controller.Play("Right Cycle");
             }
 
+            //forward animation trigger
             else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) ) 
             {
                 Controller.Play("Player Back Cycle");
             }
 
+            //back animation trigger
             else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) ) 
             {
                 Controller.Play("Front Cycle");
             }
 
+            //breathe in animation trigger
             else if (Input.GetKeyDown(KeyCode.Space))
             {
                 Controller.Play("Breathe In");
             }
 
+            //breathe out animation trigger
             else if (Input.GetKeyDown(KeyCode.M))
             {
                 Controller.Play("Breathe Out");
