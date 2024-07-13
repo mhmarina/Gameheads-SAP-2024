c7d22eed6ad4f7f2ea7efc7a74fd3215f9464882
blursified <serenacai24@gmail.com>
2024-07-12 20:31:29 -0700

    set up the main animation controller but not the script

--- Gameheads-SAP-2024/Assets/Sprites/Player/AnimationControl.cs
+++ Gameheads-SAP-2024/Assets/Sprites/Player/AnimationControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimationControl : MonoBehaviour
 {
     public Animator Controller; 
+    public bool canMove = false; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,34 +16,44 @@ public class AnimationControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) ) 
-        {
-            Controller.Play("Left Cycle");
-        }
 
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) ) 
+        if (Input.GetKeyDown(KeyCode.Space)) //change this to something else
         {
-            Controller.Play("Right Cycle");
+            canMove = true; 
         }
+        
 
-        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) ) 
-        {
-            Controller.Play("Player Back Cycle");
-        }
+        if (canMove == true)
+        { 
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) ) 
+            {
+                Controller.Play("Left Cycle");
+            }
 
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) ) 
-        {
-            Controller.Play("Front Cycle");
-        }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) ) 
+            {
+                Controller.Play("Right Cycle");
+            }
 
-        else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Controller.Play("Breathe In");
-        }
+            else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) ) 
+            {
+                Controller.Play("Player Back Cycle");
+            }
 
-        else if (Input.GetKeyDown(KeyCode.M))
-        {
-            Controller.Play("Breathe Out");
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) ) 
+            {
+                Controller.Play("Front Cycle");
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Controller.Play("Breathe In");
+            }
+
+            else if (Input.GetKeyDown(KeyCode.M))
+            {
+                Controller.Play("Breathe Out");
+            }
         }
     }
 }
