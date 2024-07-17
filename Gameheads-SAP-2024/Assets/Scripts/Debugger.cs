4d18411a99f12dcdc595e12865e8e6c42bd59528
Marina <140115877+mhmarina@users.noreply.github.com>
2024-07-17 14:36:04 -0700

    Functional Healthbar

--- Gameheads-SAP-2024/Assets/Scripts/Debugger.cs
+++ Gameheads-SAP-2024/Assets/Scripts/Debugger.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Debugger : MonoBehaviour
 {
     [SerializeField] Health playerHealth;
     [SerializeField] PlayerInventory inventory;
-    [SerializeField] TextMeshProUGUI playerHealthText;
+    [SerializeField] Image playerHealthBar;
     [SerializeField] TextMeshProUGUI inventoryText;
 
     // Update is called once per frame
     void Update()
     {
-        playerHealthText.text = "Player Health: " + playerHealth.getHealth();
+        playerHealthBar.fillAmount = playerHealth.getHealth() / (float)playerHealth.MAX_HEALTH;
         inventoryText.text = $"{inventory.getNumEnemiesCollected()} / {inventory.getGoalNumEnemies()}";
     }
 }
