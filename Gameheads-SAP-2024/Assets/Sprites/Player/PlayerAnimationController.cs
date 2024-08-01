8e75542ddabff636766fcbdcde3ce824e79fb8da
blursified <serenacai24@gmail.com>
2024-08-01 10:37:47 -0700

    put breathe animation onto player

--- Gameheads-SAP-2024/Assets/Sprites/Player/PlayerAnimationController.cs
+++ Gameheads-SAP-2024/Assets/Sprites/Player/PlayerAnimationController.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Timers;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
     public Animator Controller; 
+    public float elapsedTime = 0f; 
+    //public Animator BreatheHold; 
     // Start is called before the first frame update
     void Start()
     {
         //controller call
         Controller = GetComponent<Animator>();
+        //BreatheHold = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,11 +25,16 @@ public class PlayerAnimationController : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) 
             {
                 Controller.SetBool("Breathe In", true);
+                
+                
+                //Controller.SetBool("Breathe Out", false);
             }
 
             else
             {
                 Controller.SetBool("Breathe In", false);
+                //Controller.SetBool("Breathe Out", true);
+
             }
         
             // left animation trigger
