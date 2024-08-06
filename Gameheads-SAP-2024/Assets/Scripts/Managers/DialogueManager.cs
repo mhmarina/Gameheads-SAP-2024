c1634f6789059534556aa83e4623a2801ed820c7
Marina <140115877+mhmarina@users.noreply.github.com>
2024-08-06 14:36:59 -0700

    Tutorial Dialogue

--- Gameheads-SAP-2024/Assets/Scripts/Managers/DialogueManager.cs
+++ Gameheads-SAP-2024/Assets/Scripts/Managers/DialogueManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Trigger states
     [System.NonSerialized]
     public bool movementTrigger = false;
     public bool deadBodyTrigger = false;
@@ -16,15 +16,80 @@ public class DialogueManager : MonoBehaviour
 
     [SerializeField] Image characterSpriteImage;
     [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] GameObject dialogueBox;
+    [SerializeField] List<DialogueLine> tutorialLines;
+    [SerializeField] PlayerInventory playerInventory;
+    public static DialogueManager instance;
+    private int dialogueIndex;
 
-    void Start()
+    private void Start()
     {
+        instance = this;
+        movementTrigger = true;
+        dialogueIndex = 0;
+        ShowDialogueLine();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!inhaleTrigger && dialogueIndex == 2 && !dialogueBox.activeSelf)
+        {
+            handleDelay(0);
+            inhaleTrigger = true;
+            dialogueIndex = 3;
+            ShowDialogueLine();
+        }
+        if (inhaleTrigger && InputManager.instance.button_inhale && !releaseTrigger)
+        {
+            releaseTrigger = true;
+            dialogueIndex = 4;
+            ShowDialogueLine();
+            StartCoroutine(handleDelay(2));
+        }
+        if (playerInventory.getNumEnemiesCollected() == playerInventory.getGoalNumEnemies() && !targetNumReachedTrigger)
+        {
+            targetNumReachedTrigger = true;
+            dialogueIndex = 7;
+            ShowDialogueLine();
+        }
+    }
+
+    public void setDeadTrigger()
+    {
+        if (!deadBodyTrigger)
+        {
+            deadBodyTrigger = true;
+            dialogueIndex = 1;
+            ShowDialogueLine();
+            StartCoroutine(handleDelay(1));
+        }
+    }
+
+    private void ShowDialogueLine()
+    {
+        if (dialogueIndex >= 0 && dialogueIndex < tutorialLines.Count)
+        {
+            DialogueLine line = tutorialLines[dialogueIndex];
+            characterSpriteImage.sprite = line.charSprite;
+            dialogueText.text = line.line;
+            dialogueBox.SetActive(true);
+            playVoiceLine(line);
+        }
+    }
+
+    private IEnumerator handleDelay(int numFrames)
+    {
+        if(numFrames > 0)
+        {
+            for (int i = 1; i <= numFrames; i++)
+            {
+                yield return new WaitForSeconds(2);
+                dialogueIndex++;
+                ShowDialogueLine();
+            }
+        }
+        yield return new WaitForSeconds(3);
+        dialogueBox.SetActive(false);
     }
 
     public void playVoiceLine(DialogueLine voiceLine)
