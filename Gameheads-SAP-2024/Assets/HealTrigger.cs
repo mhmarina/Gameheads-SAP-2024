6107900ff7ed71e5dce9ba24758653dac8698bcb
blursified <serenacai24@gmail.com>
2024-08-08 21:04:54 -0700

    sript udpate

---
+++ Gameheads-SAP-2024/Assets/HealTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class HealTrigger : MonoBehaviour
+{
+
+    public Animator healTrigger;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        healTrigger = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.GetComponent<InteractableObject>())
+        {
+            Debug.Log(collision.gameObject.GetComponent<InteractableObject>().getObjectType());
+            if (collision.gameObject.GetComponent<InteractableObject>().getObjectType() == "mana" && InputManager.instance.button_inhale)
+            {
+                healTrigger.Play("Heal");
+                Debug.Log("heal");
+            }
+        }
+    }
+}
