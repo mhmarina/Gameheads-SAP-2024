7179caee7b5a2f4e6017d03999803bbd52f94158
mhmarina <marinamagedsamuel@gmail.com>
2024-08-08 22:37:02 -0700

    tones background sounds down

--- Gameheads-SAP-2024/Assets/Scripts/Managers/DialogueLine.cs
+++ Gameheads-SAP-2024/Assets/Scripts/Managers/DialogueLine.cs
@@ -17,8 +17,9 @@ public class DialogueLine
             if (GameAudioManager.instance != null)
             {
                 float currentVol = GameAudioManager.instance.musicSource.volume;
+                Debug.Log(currentVol);
                 Debug.Log("Voice line playing");
-                GameAudioManager.instance.musicSource.volume = 0.25f;
+                GameAudioManager.instance.musicSource.volume = 0.1f;
                 caller.StartCoroutine(ResetVolumeAfterVoiceLine(voiceLine.length, currentVol));
             }
         }
