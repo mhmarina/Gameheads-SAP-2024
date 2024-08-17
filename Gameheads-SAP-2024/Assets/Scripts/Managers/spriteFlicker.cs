7494fbf3738b378e4a80bc377f7e63659d8fd325
ReeseCs <102434972+Marissa-cp@users.noreply.github.com>
2024-08-17 15:37:21 -0700

    changes were made!

--- Gameheads-SAP-2024/Assets/Scripts/Managers/spriteFlicker.cs
+++ Gameheads-SAP-2024/Assets/Scripts/Managers/spriteFlicker.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class spriteFlicker : MonoBehaviour
 {
 
-    public GameObject flickerIcon;
+    [SerializeField] GameObject flickerIcon;
 
     // Start is called before the first frame update
     void Start()
